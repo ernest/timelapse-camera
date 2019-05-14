8d6d2f19d6e42529a541ae14a94ff11271214a9c
Ernest <web.ernest@outlook.com>
2019-05-14 22:27:44 +0000

    Changes to formatting

--- cam.py
+++ cam.py
@@ -1,21 +1,22 @@
-import os
-from picamera import PiCamera
 from time import sleep
 from gpiozero import LED
+
 camera = PiCamera()
 camera.resolution = camera.MAX_RESOLUTION
-primaryLed = LED(17)
-maximumPhotos = 10000 # The maximum number of pictures that will be taken
-totalPhotosTaken = 0 # Total number of photos taken, DO NOT MODIFY
-waitTime = 5 # Time to wait after each picture in seconds
-directoryToSave = "/home/pi/ketchup/"
-fileFormat = ".jpg"
-if not os.path.isfile(str(directoryToSave) + "photo 1" + str(fileFormat)):
-    while totalPhotosTaken < maximumPhotos:
-        primaryLed.on()
-        totalPhotosTaken += 1
-        camera.capture(str(directoryToSave) + "photo " + str(totalPhotosTaken) + str(fileFormat))
+
+primary_led = LED(17)
+maximum_photos = 10000  # The maximum number of pictures that will be taken
+
+total_photos_taken = 0  # Total number of photos taken, DO NOT MODIFY
+wait_time = 5  # Time to wait after each picture in seconds
+directory_to_save = "/home/pi/photos/"
+
+if not os.path.isfile(directory_to_save + "photo 1.jpg"):
+    while total_photos_taken < maximum_photos:
+        primary_led.on()
+        camera.capture(directory_to_save + "photo " + str(total_photos_taken) + ".jpg")
+        total_photos_taken += 1
         sleep(1)
-        primaryLed.off()
-        sleep(waitTime)
+        primary_led.off()
+        sleep(wait_time)
 camera.close()
