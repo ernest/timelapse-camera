5b562d85abe2ec7f379314ac1c7ed38672205d51
Ernest <web.ernest@outlook.com>
2020-02-01 02:58:35 +0000

    Cleaned up code

--- cam.py
+++ cam.py
@@ -3,20 +3,23 @@ from picamera import PiCamera
 from time import sleep
 from gpiozero import LED
 
+# The directory where the photos taken will be stored. The photos folder of the pi user is the default.
+directory_to_save = "/home/pi/photos/"
+
+MAXIMUM_PHOTOS = 10000  # The maximum number of pictures that will be taken
+total_photos_taken = 0  # Total number of photos taken
+wait_time = 5  # Time to wait after each picture in seconds
+
 camera = PiCamera()
 camera.resolution = camera.MAX_RESOLUTION
 
 primary_led = LED(17)
-maximum_photos = 10000  # The maximum number of pictures that will be taken
+primary_led.off()
 
-total_photos_taken = 0  # Total number of photos taken, DO NOT MODIFY
-wait_time = 5  # Time to wait after each picture in seconds
-directory_to_save = "/home/pi/photos/"
-
-if not os.path.isfile(directory_to_save + "photo 1.jpg"):
-    while total_photos_taken < maximum_photos:
+if not os.path.isfile(directory_to_save + "IMG_0.jpg"):
+    while total_photos_taken < MAXIMUM_PHOTOS:
         primary_led.on()
-        camera.capture(directory_to_save + "photo " + str(total_photos_taken) + ".jpg")
+        camera.capture(directory_to_save + "IMG_" + str(total_photos_taken) + ".jpg")
         total_photos_taken += 1
         sleep(1)
         primary_led.off()
